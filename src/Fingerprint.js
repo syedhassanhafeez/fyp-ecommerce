7352f229b48be6a73834201749b8e155dcccef57
unknown <Syed Hassan>
2022-01-01 16:52:46 +0500

    Updated FingerPrint Page UI

--- src/Fingerprint.js
+++ src/Fingerprint.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import firebase from './config/firebase';
-
+import './Fingerprint.scss';
 
 const Fingerprint = () => {
     const [data, setData] = useState(0);
@@ -23,26 +23,30 @@ const Fingerprint = () => {
 
     });
 
-    if (data > 100) {
+    if (data > 50) {
         setRoute();
     };
 
 
     return (
-        <div  >
-         
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-        
-            <h1 style={{marginLeft: '350px',color:'red'}}>Please Enter your Authentication using fingerprint sensor</h1>
-            <img style={{marginLeft: '500px'}} src='https://iconarchive.com/download/i96082/iconsmind/outline/Fingerprint-2.ico' alt='Some Problem' />
-        </div>
+        <>
+            <div class="animated animatedFadeInUp fadeInUp" >
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+
+                <div class="sign">
+                    <span class="fast-flicker"><h1 style={{  color: 'red' }}>Please Enter your Authentication using fingerprint sensor</h1>
+                    </span>
+                </div>
+                <img style={{ marginLeft: '850px' }} src='https://iconarchive.com/download/i96082/iconsmind/outline/Fingerprint-2.ico' alt='Some Problem' />
+            </div>
+        </>
     );
 
 }
